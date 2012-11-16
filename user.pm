43044b9b49cd8652aa489732bbf2257f6e783157
Elizabeth Anne Bartos <eab879@murphy.wot.ece.northwestern.edu>
2012-11-16 14:03:13 -0600

    formatting shtuffs

---
+++ user.pm
@@ -0,0 +1,13 @@
+#!/usr/bin/perl -
+package user;
+our @ISA = 'Exporter';
+our @EXPORT = qw(   $netID
+                    $dbuser
+                    $dbpasswd
+                    );
+
+use Exporter;
+
+our $netID = "eab879";
+our $dbuser = "eab879";
+our $dbpasswd = "w67iYahH"; 
